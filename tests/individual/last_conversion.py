876a7d6094e4294f413c48cb549e99b0f27701bb
Shahin ABDI <contact@shahinabdi.fr>
2024-12-10 23:41:07 +0100

    chore: TODO 5 last conversion test

---
+++ tests/individual/last_conversion.py
@@ -0,0 +1,14 @@
+import pytest
+from src.converter import CurrencyConverter
+
+# TODO 5: Create tests for last_conversion functionality
+
+
+def test_last_conversion_initial_state(converter):
+    """Test initial state of last_conversion"""
+    pass
+
+
+def test_last_conversion_updates(converter):
+    """Test last_conversion updates correctly"""
+    pass
