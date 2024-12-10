ac0ad469f6ba6a8906447ab0436d104976168f29
Shahin ABDI <contact@shahinabdi.fr>
2024-12-10 23:47:15 +0100

    chore: TODO 6 parametrize same currency

---
+++ tests/individual/same_currency.py
@@ -0,0 +1,12 @@
+import pytest
+from src.converter import CurrencyConverter
+
+# TODO 6: Create tests for same currency conversions
+
+
+@pytest.mark.parametrize("currency", [
+    # Add test cases for same currency conversion
+])
+def test_same_currency_conversion(converter):
+    """Test conversion between same currencies"""
+    pass
