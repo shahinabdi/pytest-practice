593c7a93bfefa63a0fe3aa629f1b89104af7dfa6
Shahin ABDI <contact@shahinabdi.fr>
2024-12-10 23:36:19 +0100

    chore: TODO 3 parametrize edge cases

---
+++ tests/individual/edge_cases.py
@@ -0,0 +1,11 @@
+import pytest
+from src.converter import CurrencyConverter
+
+
+@pytest.mark.parametrize("amount", [
+    # TODO 3: Add test cases for edge amounts
+    # Consider: zero, very small numbers, very large numbers
+])
+def test_edge_case_amounts(converter):
+    """Test edge case amounts"""
+    pass
